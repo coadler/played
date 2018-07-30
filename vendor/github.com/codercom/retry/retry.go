7fad2b5f266a92ea394c90ae930efc50829f65be
Colin <colin.adler@yahoo.com>
2018-07-29 23:14:43 -0500

    Dep ensure

---
+++ vendor/github.com/codercom/retry/retry.go
@@ -0,0 +1,227 @@
+// Package retry contains utilities for retrying an action until it succeeds.
+package retry
+
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+// Retry holds state about a retryable operation.
+// Callers should create this via New.
+type Retry struct {
+	sleepDur func() time.Duration
+
+	// preConditions are ran before each call to fn.
+	preConditions []func() bool
+
+	// postConditions are ran after each call to fn.
+	postConditions []Condition
+}
+
+// New creates a new retry.
+// The default retry will run forever, sleeping sleep.
+func New(sleep time.Duration) *Retry {
+	r := &Retry{
+		sleepDur: func() time.Duration {
+			return sleep
+		},
+	}
+
+	r.appendPostConditions(func(err error) bool {
+		return err != nil
+	})
+
+	return r
+}
+
+func (r *Retry) appendPreCondition(fn func() bool) {
+	r.preConditions = append(r.preConditions, fn)
+}
+func (r *Retry) appendPostConditions(fns ...Condition) {
+	for _, fn := range fns {
+		r.postConditions = append(r.postConditions, fn)
+	}
+}
+
+// Condition is a function that decides based on the given error whether to retry.
+type Condition func(error) bool
+
+// OnErrors returns a condition which retries on one of the provided errors.
+func OnErrors(errs ...error) Condition {
+	return func(err error) bool {
+		for _, checkErr := range errs {
+			if errors.Cause(err) == checkErr {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// NotOnErrors returns a condition which retries only if the error
+// does not match one of the provided errors.
+func NotOnErrors(errs ...error) Condition {
+	return func(err error) bool {
+		for _, checkErr := range errs {
+			if errors.Cause(err) == checkErr {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Condition appends the passed retry conditions.
+// All conditions must return true for the retry to progress.
+func (r *Retry) Conditions(fns ...Condition) *Retry {
+	r.appendPostConditions(fns...)
+	return r
+}
+
+// Condition appends the passed retry condition.
+// The condition must return true for the retry to progress.
+// Deprecated: Use Conditions instead.
+func (r *Retry) Condition(fn Condition) *Retry {
+	r.appendPostConditions(fn)
+	return r
+}
+
+func (r *Retry) preCheck() bool {
+	for _, fn := range r.preConditions {
+		if !fn() {
+			return false
+		}
+	}
+	return true
+}
+
+func (r *Retry) postCheck(err error) bool {
+	for _, fn := range r.postConditions {
+		if !fn(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// Attempts sets the maximum amount of retry attempts
+// before the current error is returned.
+func (r *Retry) Attempts(n int) *Retry {
+	var iterations int
+	r.appendPreCondition(func() bool {
+		ok := iterations < n
+		iterations++
+		return ok
+	})
+	return r
+}
+
+// Context bounds the retry to when the context expires.
+func (r *Retry) Context(ctx context.Context) *Retry {
+	r.appendPreCondition(func() bool {
+		return ctx.Err() == nil
+	})
+	return r
+}
+
+// Backoff turns retry into an exponential backoff
+// with a maximum sleep of ceil.
+func (r *Retry) Backoff(ceil time.Duration) *Retry {
+	const growth = 2
+
+	// start delay at half so that
+	// the first iteration of sleepDur doubles it.
+	delay := r.sleepDur() / growth
+
+	if delay == 0 {
+		panic("retry: delay must not be zero (is it less than 2 nanoseconds?) ")
+	}
+
+	r.sleepDur = func() time.Duration {
+		if delay < ceil {
+			delay = delay * growth
+			if delay > ceil {
+				delay = ceil
+			}
+		}
+		return delay
+	}
+
+	return r
+}
+
+// Timeout returns the retry with a bounding timeout.
+// If the passed timeout is 0, Timeout does nothing. This has been done
+// to match the behaviour of the previous retry API and to make it easy
+// for functions that call into retry to offer optional timeouts.
+func (r *Retry) Timeout(to time.Duration) *Retry {
+	if to == 0 {
+		return r
+	}
+
+	deadline := time.Now().Add(to)
+
+	r.appendPreCondition(func() bool {
+		return time.Now().Before(deadline)
+	})
+
+	return r
+}
+
+// Jitter adds some random jitter to the retry's sleep.
+//
+// Ratio must be between 0 and 1, and determines how jittery
+// the sleeps will be. For example, a rat of 0.1 and a sleep of 1s restricts the
+// jitter to the range of 900ms to 1.1 seconds.
+func (r *Retry) Jitter(rat float64) *Retry {
+	if !(rat < 1 && rat > 0) {
+		panic("retry: rat must be (0, 1)")
+	}
+
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	underlyingSleep := r.sleepDur
+	r.sleepDur = func() time.Duration {
+		dur := underlyingSleep()
+
+		var (
+			minDuration = float64(dur) * (1 - rat)
+			maxDuration = float64(dur) * (1 + rat)
+		)
+
+		dur = time.Duration(minDuration) + time.Duration(rnd.Int63n(int64(maxDuration)-int64(minDuration)))
+		return dur
+	}
+
+	return r
+}
+
+// Log adds a function to log any returned errors.
+// It is added as a post condition that always returns true.
+// If you want an error to stop the retry and not be logged,
+// use Log() after the Condition.
+// If you want an error to stop the retry and be logged,
+// use Log() before the Condition.
+func (r *Retry) Log(logFn func(error)) *Retry {
+	return r.Condition(func(err error) bool {
+		logFn(err)
+		return true
+	})
+}
+
+// Run runs the retry.
+// The retry must not be ran twice.
+func (r *Retry) Run(fn func() error) error {
+	err := errors.Errorf("didn't run a single iteration?")
+	for r.preCheck() {
+		err = fn()
+		if !r.postCheck(err) {
+			return err
+		}
+		time.Sleep(r.sleepDur())
+	}
+	return err
+}
---
+++ vendor/github.com/codercom/retry/retry_test.go
@@ -0,0 +1,200 @@
+package retry
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRetry(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Attempts", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("Respects count and sleeps between attempts", func(t *testing.T) {
+			t.Parallel()
+
+			count := 0
+			start := time.Now()
+
+			const sleep = time.Millisecond * 10
+
+			New(sleep).Attempts(5).Run(func() error {
+				count++
+				return errors.Errorf("asdfasdf")
+			})
+
+			assert.Equal(t, 5, count)
+			assert.WithinDuration(t, start.Add(sleep*5), time.Now(), sleep)
+		})
+
+		t.Run("returns as soon as error is nil", func(t *testing.T) {
+			t.Parallel()
+
+			start := time.Now()
+
+			var count int
+			New(time.Minute).Attempts(100).Run(func() error {
+				count++
+				return nil
+			})
+
+			assert.Equal(t, 1, count)
+			assert.WithinDuration(t, time.Now(), start, time.Millisecond)
+		})
+	})
+
+	t.Run("Backoff", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("return when nil", func(t *testing.T) {
+			t.Parallel()
+
+			var count int
+			err := New(time.Millisecond).Timeout(time.Minute).Backoff(time.Second).Run(func() error {
+				count++
+				if count == 10 {
+					return nil
+				}
+				return io.EOF
+			})
+
+			assert.Equal(t, 10, count)
+			assert.NoError(t, err)
+		})
+	})
+
+	t.Run("Timeout", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("Respects timeout and sleeps between attempts", func(t *testing.T) {
+			t.Parallel()
+			count := 0
+			start := time.Now()
+
+			const sleep = time.Millisecond * 10
+
+			New(sleep).Timeout(sleep * 5).Run(func() error {
+				count++
+				return errors.Errorf("asdfasdf")
+			})
+
+			assert.Equal(t, 5, count)
+			assert.WithinDuration(t, start.Add(sleep*5), time.Now(), sleep)
+		})
+
+		t.Run("returns as soon as error is nil", func(t *testing.T) {
+			t.Parallel()
+			start := time.Now()
+
+			New(time.Minute).Timeout(time.Hour).Run(func() error {
+				return nil
+			})
+
+			assert.WithinDuration(t, time.Now(), start, time.Millisecond*10)
+		})
+	})
+
+	t.Run("Cond", func(t *testing.T) {
+		t.Parallel()
+		count := 0
+
+		err := New(time.Millisecond).
+			Condition(NotOnErrors(io.ErrShortWrite)).
+			Condition(OnErrors(io.ErrUnexpectedEOF, io.EOF, io.ErrShortWrite)).
+			Run(func() error {
+				count++
+				if count == 5 {
+					return io.ErrShortWrite
+				}
+				if count%2 == 0 {
+					return errors.WithStack(io.ErrUnexpectedEOF)
+				}
+				return io.EOF
+			})
+
+		assert.Equal(t, 5, count)
+		assert.Equal(t, io.ErrShortWrite, err)
+
+	})
+
+	t.Run("Context", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+
+		var count int
+
+		err := New(time.Millisecond).Context(ctx).Run(func() error {
+			if count == 3 {
+				cancel()
+			} else {
+				count++
+			}
+			return io.EOF
+		})
+
+		assert.Equal(t, 3, count)
+		assert.Equal(t, io.EOF, err)
+	})
+
+	t.Run("Jitter", func(t *testing.T) {
+		t.Parallel()
+		var count int
+
+		var durs []time.Duration
+		last := time.Now()
+		New(time.Millisecond).Attempts(500).Jitter(0.9991).Run(func() error {
+			durs = append(durs, time.Since(last))
+			last = time.Now()
+			count++
+			return io.EOF
+		})
+
+		avg := avgDurations(durs)
+
+		t.Logf("avg dur: %v", avg)
+
+		if avg < time.Microsecond*900 || avg > time.Microsecond*1200 {
+			t.Errorf("bad avg dur")
+		}
+	})
+
+	t.Run("Log", func(t *testing.T) {
+		t.Parallel()
+
+		err := errors.New("meow")
+		n := 0
+
+		New(time.Millisecond).Log(func(e error) {
+			require.Equal(t, err, e)
+		}).Run(func() error {
+			n++
+
+			// indexed from one :(
+			switch n {
+			case 1:
+				return err
+			case 2:
+				return nil
+			}
+
+			t.Fatal("should not reach this line.")
+			panic("?")
+		})
+	})
+}
+
+func avgDurations(durs []time.Duration) time.Duration {
+	var sum time.Duration
+	for _, dur := range durs {
+		sum += dur
+	}
+	return sum / time.Duration(len(durs))
+}
